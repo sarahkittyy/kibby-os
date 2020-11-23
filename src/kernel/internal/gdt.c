310890f40bf6b9c4ff1d2e394621720f2615d621
Sarah Ohlin <sarahohlin1@gmail.com>
2020-11-23 03:34:48 -0500

    feat: additional bitfields for easy gdt entry manip

--- src/kernel/internal/gdt.c
+++ src/kernel/internal/gdt.c
@@ -1,23 +1,27 @@
 #include "gdt.h"
 
+#include "kernel/std/assert.h"
+
 gdt_t kibby_gdt[3];
 gdt_table_t kibby_gdt_table;
 
 // https://wiki.osdev.org/LGDT
 void gdt_set_gate(uint32_t id, uint32_t base, uint32_t lim, uint8_t acc, uint8_t gran) {
-	kibby_gdt[id].base_lo  = (base & 0xFFFF);
-	kibby_gdt[id].base_mid = (base >> 16) & 0xFF;
-	kibby_gdt[id].base_hi  = (base >> 24) & 0xFF;
+	kibby_gdt[id].sl.base_lo  = (base & 0xFFFF);
+	kibby_gdt[id].sl.base_mid = (base >> 16) & 0xFF;
+	kibby_gdt[id].sl.base_hi  = (base >> 24) & 0xFF;
 
-	kibby_gdt[id].limit_lo = (lim & 0xFFFF);
-	kibby_gdt[id].gran	   = (lim >> 16) & 0x0F;
+	kibby_gdt[id].sl.limit_lo = (lim & 0xFFFF);
+	kibby_gdt[id].sl.gran	   = (lim >> 16) & 0x0F;
 
-	kibby_gdt[id].gran |= gran & 0xF0;
-	kibby_gdt[id].access = acc;
+	kibby_gdt[id].sl.gran |= gran & 0xF0;
+	kibby_gdt[id].sl.access = acc;
 }
 
 void setup_gdt() {
-	kibby_gdt_table.sz	 = (sizeof(struct gdt) * 3) - 1;
+	assert(sizeof(gdt_t) == 8);
+
+	kibby_gdt_table.sz	 = (sizeof(gdt_t) * 3) - 1;
 	kibby_gdt_table.addr = (uint32_t)&kibby_gdt;
 
 	gdt_set_gate(0, 0, 0, 0, 0);				  // null
--- src/kernel/internal/gdt.h
+++ src/kernel/internal/gdt.h
@@ -9,19 +9,41 @@ void setup_gdt();
 void gdt_set_gate(uint32_t id, uint32_t base, uint32_t lim, uint8_t acc, uint8_t gran);
 
 // gdt entries are 8-byte entries
-struct gdt {
-	uint16_t limit_lo;
+typedef union {
+	struct {
+		uint16_t limit_lo;
 
-	uint16_t base_lo;
-	uint8_t base_mid;
+		uint16_t base_lo;
+		uint8_t base_mid;
 
-	uint8_t access;
+		uint8_t access;
 
-	uint8_t gran;
+		uint8_t gran;
 
-	uint8_t base_hi;
-} __attribute__((packed));
-typedef struct gdt gdt_t;
+		uint8_t base_hi;
+	} __attribute__((packed)) sl;
+	struct {
+		uint16_t limit_lo: 16; // lower 16 bits of limit
+		uint32_t base_lo: 24; // lower 24 bits of base
+
+		uint8_t accd: 1; // cpu sets this to one when accessed
+		uint8_t rw: 1; // allow read for code selectors, allow write for data selectors
+		uint8_t dc: 1; // data: 0 grows up, 1 grows down
+		uint8_t ex: 1; // executable bit
+		uint8_t s: 1; // set for code/data segments, cleared for sys segs
+		uint8_t priv: 2; // ring level 0-3
+		uint8_t p: 1; // present bit (must be 1
+
+		uint8_t lim_hi: 4; // high nibble of limit
+
+		uint8_t zero: 2;
+		uint8_t gran: 1; // 1 for 4KiB granularity, 0 for byte granularity
+		uint8_t sz: 1; // 0 - 16 bit, 1 = 32 bit
+
+		uint8_t base_hi: 8;
+	} __attribute__((packed)) ss;
+	uint64_t b;
+} gdt_t;
 
 struct gdt_table {
 	uint16_t sz;
