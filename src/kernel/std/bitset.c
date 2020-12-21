0f2cf5ea6f106c8ccfc41d49fa6133a79735a659
Sarah Ohlin <sarahohlin1@gmail.com>
2020-12-21 18:49:58 -0500

    fix: bitmap issues

--- src/kernel/std/bitset.c
+++ src/kernel/std/bitset.c
@@ -1,6 +1,7 @@
 #include "bitset.h"
 
 #include <kernel/internal/kmem.h>
+#include <kernel/std/string.h>
 
 static size_t bitset_major_index(size_t b) {
 	return b / 8;
@@ -11,10 +12,11 @@ static size_t bitset_minor_index(size_t b) {
 }
 
 bitset_t* bitset_create(size_t bits) {
-	bitset_t* b = (bitset_t*)kmalloc_old(sizeof(bitset_t));
+	bitset_t* b = (bitset_t*)kmalloc(sizeof(bitset_t));
 	// 1 byte / 8 bits
-	b->data = (uint8_t*)kmalloc_old(bits / 8 + 1);
-	b->sz	= bits;
+	b->data = (uint8_t*)kmalloc(bits / 8 + 1);
+	memset(b->data, 0, bits / 8 + 1);
+	b->sz = bits;
 	return b;
 }
 
@@ -37,7 +39,7 @@ size_t bitset_first_unset(bitset_t* s) {
 	// for every byte
 	for (size_t i = 0; i < s->sz / 8 + 1; ++i) {
 		// if none are set in the whole byte, ignore
-		if (!(i & 0xFF)) {
+		if (s->data[i] == 0xFF) {
 			continue;
 		} else {
 			// check all bits in this byte
